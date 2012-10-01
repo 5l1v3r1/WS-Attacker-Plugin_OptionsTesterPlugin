6ed67b523a375d224f3a9ec5603f31231ecf4644
Christian <christian@t430s>
2012-10-01 13:13:37 +0000

    MAVEN Support. SAML over SOAP for XSW. Some GUI Improvements

--- src/main/java/wsattacker/plugin/optionsTester/OptionsTesterPlugin.java
+++ src/main/java/wsattacker/plugin/optionsTester/OptionsTesterPlugin.java
@@ -1,3 +1,21 @@
+/**
+ * WS-Attacker - A Modular Web Services Penetration Testing Framework
+ * Copyright (C) 2012 Christian Mainka
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package wsattacker.plugin.optionsTester;
 import wsattacker.main.composition.plugin.AbstractPlugin;
 import wsattacker.main.composition.plugin.option.AbstractOption;
@@ -5,7 +23,6 @@ import wsattacker.main.composition.testsuite.RequestResponsePair;
 import wsattacker.main.plugin.PluginOptionContainer;
 import wsattacker.main.plugin.PluginState;
 import wsattacker.main.plugin.option.OptionSimpleBoolean;
-import wsattacker.main.plugin.option.OptionSimpleFile;
 import wsattacker.main.plugin.option.OptionSimpleText;
 import wsattacker.main.plugin.option.OptionSimpleVarchar;
 
